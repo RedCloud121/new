5db9faf36730c30998726ff21797e3fa7cdbea96
RedCloud121 <41609282+RedCloud121@users.noreply.github.com>
2018-08-24 00:13:14 +1200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -13,6 +13,11 @@ pip install botocore boto boto3
 mkdir -pv ~/.aws/
 
 #set aws asscess key and aws secret access key
+
+read -p "Your AWS_Access_Key_ID is:" aaki
+read -p "Your AWS_Secret_Access_Key is:" asak
+read -p "Your Region(eg.us-west-2) is:" region
+
 echo "[default]
 aws_access_key_id = $1
 aws_secret_access_key = $2" > ~/.aws/credentials
